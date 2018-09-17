99e7a1b7d05563241c9cbe989e55e4180db8f2d9
Joshua Mack <mackncheesiest@gmail.com>
2018-09-17 09:57:26 -0700

    WIP min EFT computation + scheduling

--- heft/heft.py
+++ heft/heft.py
@@ -1,6 +1,7 @@
 """Core code to be used for scheduling a given DAG"""
 
-from collections import deque
+from collections import deque, namedtuple
+from math import inf
 
 import argparse
 import logging
@@ -11,9 +12,11 @@ import networkx as nx
 
 logger = logging.getLogger('heft')
 
+ScheduleEvent = namedtuple('ScheduleEvent', 'task start end')
+
 class HEFT_Environment:
     """
-    An object representing a HEFT environment. Consists of all features that are relevant for scheduling an arbitrary DAG G
+    An object representing a HEFT environment. Consists of all features that are relevant for scheduling an arbitrary DAG
     """
 
     """
@@ -46,6 +49,9 @@ class HEFT_Environment:
     def __init__(self, computation_matrix=W0, communication_matrix=C0):
         self.computation_matrix = computation_matrix
         self.communication_matrix = communication_matrix
+        self.job_schedules = {}
+        for i in range(1, len(self.computation_matrix)+1):
+            self.job_schedules[i] = None
 
     def compute_ranku(self, dag, terminal_node):
         nx.set_node_attributes(dag, { terminal_node: np.mean(self.computation_matrix[terminal_node-1]) }, "ranku")
@@ -60,13 +66,20 @@ class HEFT_Environment:
                 val = float(dag[node][succnode]['weight']) + dag.nodes()[succnode]['ranku']
                 if val > max_successor_ranku:
                     max_successor_ranku = val
-            assert max_successor_ranku > 0, "Expected maximum successor ranku to be greater than 0 but was %r" % max_successor_ranku
+            assert max_successor_ranku > 0, "Expected maximum successor ranku to be greater than 0 but was %s" % max_successor_ranku
             nx.set_node_attributes(dag, { node: np.mean(self.computation_matrix[node-1]) + max_successor_ranku }, "ranku")
 
             visit_queue.extendleft([prednode for prednode in dag.predecessors(node) if prednode not in visit_queue])
         
         for node in dag.nodes():
             logger.info("Node: %s, Rank U: %s" % (node, dag.nodes()[node]['ranku']))
+
+    def compute_eft(self, dag, node, proc):
+        readytime = 0
+        for prednode in dag.predecessors(node):
+            assert self.job_schedules[prednode] != None, "Predecessor nodes must be scheduled before their children"
+            readytimetemp = self.job_schedules[prednode].end + self.communication_matrix
+
 
     def schedule_dag(self, dag):
         # Nodes with no successors cause the any expression to be empty
@@ -78,7 +91,12 @@ class HEFT_Environment:
 
         sorted_nodes = sorted(dag.nodes(), key=lambda node: dag.nodes()[node]['ranku'], reverse=True)
         for node in sorted_nodes:
-            minEFT = sys.maxsize
+            minTaskSchedule = ScheduleEvent(node, inf, inf)
+            for proc in range(1, len(self.computation_matrix[node-1])+1):
+                taskschedule = self.compute_eft(dag, node, proc)
+                if (taskschedule.end < minTaskSchedule.end):
+                    minTaskSchedule = taskschedule
+            self.job_schedules[proc].append(minTaskSchedule)
             #for p in range(len(self.computation_matrix[node-1])):
             #TODO: Implement EFT(ni, pk) using insertion-based scheduling policy
 
@@ -93,7 +111,7 @@ def readDagMatrix(dag_file, show_dag=False):
             # All edges with weight of 0
             [edge for edge in dag.edges() if dag.get_edge_data(*edge)['weight'] == '0']
         )
-        # Change 0-based node indexing to 1-based
+        # Change 0-based node labels to 1-based
         dag = nx.relabel_nodes(dag, dict(map(lambda node: (node, node+1), list(dag.nodes()))))
 
         if show_dag:
